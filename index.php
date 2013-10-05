4ee158271a4dc966c11a3b5516503dd34b736b11
DaveThePianoDude <dah_7@yahoo.com>
2013-10-04 21:48:18 -0400

    added more stuff

--- index.php
+++ index.php
@@ -26,7 +26,36 @@
 	
 	<?php
 	
-	phpinfo();
+		function pg_connection_string() {
+
+			return "dbname=d7qq84ps7u5thb host=ec2-184-73-175-240.compute-1.amazonaws.com port=5432 user=jnnvxxjaenvzor password=Xpq6UHZoub1e6LIUPdUZrX6bSz sslmode=require";
+		
+		}
+
+		# Establish db connection
+		$db = pg_connect(pg_connection_string());
+		
+		if (!$db) {
+		
+			echo "Database connection error.";
+			exit;
+			
+		} else
+		{
+			echo "Database connection succeeded.";
+			
+			echo "Now 'n' Then v. 0.123";
+			
+			$query = "CREATE TABLE IF NOT EXISTS Places (location_id bigserial primary key, name varchar(20) NOT NULL, lat varchar(50) NOT NULL, lon varchar(50) NOT NULL);";
+			
+			pg_query($db, $query);
+			
+			echo "got here";	
+		}
+	
+		$query = "INSERT INTO Places VALUES (2, 'SOMEWHERE', '300231.39293', '3919293.221');";
+		pg_query($db, $query);
+		echo "inserted row";
 	
 	?>
 	

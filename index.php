02f9e6431bb693676d2a200b33a9429ef7c6b4f0
DaveThePianoDude <dah_7@yahoo.com>
2013-10-12 15:55:51 -0400

    Folded in slider.

--- index.php
+++ index.php
@@ -15,8 +15,127 @@
 			border-color:red }
 	
     </style>
+	
+	<style type="text/css">
+		#box {
+			width: 200px; height: 200px; background-color: #ff0000; }
+			
+		#slider { width: 15px; }
+		
+		#slider-bg {
+			background:url(http://yui.yahooapis.com/2.9.0/build/slider/assets/bg-fader.gif) 5px 0 no-repeat;
+		}
+		#slider-thumb {
+			left: 0;
+		}
+	</style>
 
 	<script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
+	
+	<script type="text/javascript" src="http://yui.yahooapis.com/combo?2.9.0/build/yahoo-dom-event/yahoo-dom-event.js&2.9.0/build/dragdrop/dragdrop-min.js&2.9.0/build/slider/slider-min.js"></script>
+	
+	<script type="text/javascript">
+		
+			(function() {
+				var Event = YAHOO.util.Event,
+					Dom   = YAHOO.util.Dom,
+					lang  = YAHOO.lang,
+					slider, 
+					bg="slider-bg", thumb="slider-thumb", 
+					valuearea="slider-value", textfield="slider-converted-value"
+
+				// The slider can move 0 pixels up
+				var topConstraint = 0;
+
+				// The slider can move 200 pixels down
+				var bottomConstraint = 200;
+
+				// Custom scale factor for converting the pixel offset into a real value
+				var scaleFactor = 1.5;
+
+				// The amount the slider moves when the value is changed with the arrow
+				// keys
+				//var keyIncrement = 20;
+
+				//var tickSize = 20;
+
+				Event.onDOMReady(function() {
+
+					slider = YAHOO.widget.Slider.getHorizSlider(bg, 
+									 thumb, topConstraint, bottomConstraint, 0);
+
+					// Sliders with ticks can be animated without YAHOO.util.Anim
+					slider.animate = true;
+
+					slider.getRealValue = function() {
+						return Math.round(this.getValue() * scaleFactor);
+					}
+
+					slider.subscribe("change", function(offsetFromStart) {
+
+						var valnode = Dom.get(valuearea);
+						var fld = Dom.get(textfield);
+
+						// Display the pixel value of the control
+						valnode.innerHTML = offsetFromStart;
+
+						// use the scale factor to convert the pixel offset into a real
+						// value
+						var actualValue = slider.getRealValue();
+
+						// update the text box with the actual value
+						
+						
+						//fld.value = actualValue;
+
+						// Update the title attribute on the background.  This helps assistive
+						// technology to communicate the state change
+						
+						
+						//Dom.get(bg).title = "slider value = " + actualValue;
+
+					});
+
+					slider.subscribe("slideStart", function() {
+							YAHOO.log("slideStart fired", "warn");
+						});
+
+					slider.subscribe("slideEnd", function() {
+							YAHOO.log("slideEnd fired", "warn");
+						});
+
+					// Listen for keystrokes on the form field that displays the
+					// control's value.  While not provided by default, having a
+					// form field with the slider is a good way to help keep your
+					// application accessible.
+					Event.on(textfield, "keydown", function(e) {
+
+						// set the value when the 'return' key is detected
+						if (Event.getCharCode(e) === 13) {
+							var v = parseFloat(this.value, 10);
+							v = (lang.isNumber(v)) ? v : 0;
+
+							// convert the real value into a pixel offset
+							slider.setValue(Math.round(v/scaleFactor));
+						}
+					});
+					
+					// Use setValue to reset the value to white:
+					Event.on("putval", "click", function(e) {
+						slider.setValue(100, false); //false here means to animate if possible
+					});
+					
+					// Use the "get" method to get the current offset from the slider's start
+					// position in pixels.  By applying the scale factor, we can translate this
+					// into a "real value
+					Event.on("getval", "click", function(e) {
+						YAHOO.log("Current value: "   + slider.getValue() + "\n" + 
+								  "Converted value: " + slider.getRealValue(), "info", "example"); 
+					});
+				});
+			})();
+			
+	</script>
 
   </head>
   
@@ -150,6 +269,7 @@
 	pg_free_result($result);
 	
 	pg_close($db);
+	
 	?>
 </html>
 

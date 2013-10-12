3a2c92cb180d9aac31f91de82e4cc24c1a3e7be0
DaveThePianoDude <dah_7@yahoo.com>
2013-10-12 12:56:33 -0400

    More

--- index.php
+++ index.php
@@ -123,8 +123,7 @@
 	
 	$uid = pg_num_rows($result);
 	
-//	$result = pg_query($db, "SELECT * FROM NOW_PHOTOS WHERE id = '$uid'");
-	$result = pg_query($db, "SELECT * FROM NOW_PHOTOS");
+	$result = pg_query($db, "SELECT * FROM NOW_PHOTOS WHERE id = '$uid'");
 
 	echo("<table border=2><tr><td>UID</td><td>DATA</td></tr>");
 	

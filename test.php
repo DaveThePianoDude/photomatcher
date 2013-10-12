c5b2af5c77499bf1e5486bd083e7b30344482ed7
DaveThePianoDude <dah_7@yahoo.com>
2013-10-12 12:15:16 -0400

    More

--- test.php
+++ test.php
@@ -1,26 +1,11 @@
 <?php
 
-	function pg_connection_string() {
-
-		return "dbname=d7qq84ps7u5thb host=ec2-184-73-175-240.compute-1.amazonaws.com port=5432 user=jnnvxxjaenvzor password=Xpq6UHZoub1e6LIUPdUZrX6bSz sslmode=require";
-	}
+	if (isset($_FILES['uploadedfile']))
 	
-	# Establish db connection
-	//$db = pg_connect(pg_connection_string());
+		echo 'FILE EXISTS';
+		
+	else
 	
-	// # Get the image file data
-	$uploadedfile = pg_escape_bytea($_FILES['uploadedfile']['tmp_name']));
+		echo 'FILE DOES NOT EXIST';
 	
-	// open it
-	//$fp = fopen($uploadedfile,'r');
-	
-	// read it
-	//$data = fread($fp, filesize($uploadedfile));
-	
-	//fclose($fp);
-	
-	//pg_close($db);
-	
-	// return it
-	echo 'HELLO';
 ?>

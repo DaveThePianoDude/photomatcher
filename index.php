8cf3b8268e65a0597c4af7f3fc75f7dd2adfc971
DaveThePianoDude <dah_7@yahoo.com>
2013-10-12 09:46:19 -0400

    More

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 
 	$result = pg_query($db, "SELECT * FROM NOW_PHOTOS");
 
-	$image = pg_unescape_bytea(pg_result($result, 0, 1));
+	$image = pg_unescape_bytea(pg_result($result, 0, 0));
 	
 	echo "$image";
 	

c2b07c44acf956541ef627b75774224b96190b6d
DaveThePianoDude <dah_7@yahoo.com>
2013-10-11 20:18:37 -0400

    Added more image processing.

--- test.php
+++ test.php
@@ -5,6 +5,23 @@
 		return "dbname=d7qq84ps7u5thb host=ec2-184-73-175-240.compute-1.amazonaws.com port=5432 user=jnnvxxjaenvzor password=Xpq6UHZoub1e6LIUPdUZrX6bSz sslmode=require";
 	}
 	
+	# Establish db connection
+	$db = pg_connect(pg_connection_string());
+	
+	if (!$db) {
+	
+		echo "Database connection error.";
+		exit;
+		
+	} else
+	{
+		echo "Database connection succeeded.";
+		
+		$query = "CREATE TABLE IF NOT EXISTS NOW_PHOTOS(id INT PRIMARY KEY, data BYTEA);";
+		
+		pg_query($db, $query);
+	}
+
 	//$uploaddir = 'uploads/';
 	$file = basename($_FILES['uploadedfile']['name']);
 	//$uploadfile = $uploaddir . $file;
@@ -20,25 +37,7 @@
 		echo "error";
 	}
 
-	# Establish db connection
-	$db = pg_connect(pg_connection_string());
 	
-	if (!$db) {
-	
-		echo "Database connection error.";
-		exit;
-		
-	} else
-	{
-		echo "Database connection succeeded.";
-		
-		echo "Now 'n' Then v. 0.124";
-		
-		$query = "CREATE TABLE IF NOT EXISTS NOW_PHOTOS(id INT PRIMARY KEY, data BYTEA);";
-		
-		pg_query($db, $query);
-	}
-
 	$img = fopen($uploadfile, 'r') or die("cannot read image\n");
 	$data = fread($img, filesize($uploadfile));
 

6a9e215a2ea8403d842f32c0606daa1862e404c2
DaveThePianoDude <dah_7@yahoo.com>
2013-10-14 20:18:08 -0400

    Added stuff to restart slider.

--- index.php
+++ index.php
@@ -7,6 +7,78 @@
 	
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
 
+	<script type="text/javascript">
+			
+		function reslide() {
+		
+					alert ('hello');
+
+					slider = YAHOO.widget.Slider.getHorizSlider(bg, 
+									 thumb, topConstraint, bottomConstraint, 0);
+
+					// Sliders with ticks can be animated without YAHOO.util.Anim
+					slider.animate = true;
+
+					slider.getRealValue = function() {
+						return Math.round(this.getValue() * scaleFactor);
+					}
+
+					slider.subscribe("change", function(offsetFromStart) {
+
+						var valnode = Dom.get(valuearea);
+						var fld = Dom.get(textfield);
+						var pic = Dom.get("thenImage12");
+						
+						valnode.innerHTML = offsetFromStart;
+
+						var actualValue = slider.getRealValue();
+		
+						pic.style.opacity = actualValue / 290;
+									
+						Dom.get(bg).title = "slider value = " + actualValue;
+					});
+
+					slider.subscribe("slideStart", function() {
+							YAHOO.log("slideStart fired", "warn");
+						});
+
+					slider.subscribe("slideEnd", function() {
+							YAHOO.log("slideEnd fired", "warn");
+						});
+
+					// Listen for keystrokes on the form field that displays the
+					// control's value.  While not provided by default, having a
+					// form field with the slider is a good way to help keep your
+					// application accessible.
+					Event.on(textfield, "keydown", function(e) {
+
+						// set the value when the 'return' key is detected
+						if (Event.getCharCode(e) === 13) {
+							var v = parseFloat(this.value, 10);
+							v = (lang.isNumber(v)) ? v : 0;
+
+							// convert the real value into a pixel offset
+							slider.setValue(Math.round(v/scaleFactor));
+						}
+					});
+					
+					// Use setValue to reset the value to white:
+					Event.on("putval", "click", function(e) {
+						slider.setValue(100, false); //false here means to animate if possible
+					});
+					
+					// Use the "get" method to get the current offset from the slider's start
+					// position in pixels.  By applying the scale factor, we can translate this
+					// into a "real value
+					Event.on("getval", "click", function(e) {
+						YAHOO.log("Current value: "   + slider.getValue() + "\n" + 
+								  "Converted value: " + slider.getRealValue(), "info", "example"); 
+					});
+				}
+		
+	
+	</script>
+	
 	<style type="text/css" media="screen">
 	
 	#map { height: 500px;
@@ -177,74 +249,6 @@
 	
 	?>
 		<script type="text/javascript">
-		
-		function reslide() {
-		
-					alert ('hello');
-
-					slider = YAHOO.widget.Slider.getHorizSlider(bg, 
-									 thumb, topConstraint, bottomConstraint, 0);
-
-					// Sliders with ticks can be animated without YAHOO.util.Anim
-					slider.animate = true;
-
-					slider.getRealValue = function() {
-						return Math.round(this.getValue() * scaleFactor);
-					}
-
-					slider.subscribe("change", function(offsetFromStart) {
-
-						var valnode = Dom.get(valuearea);
-						var fld = Dom.get(textfield);
-						var pic = Dom.get("thenImage12");
-						
-						valnode.innerHTML = offsetFromStart;
-
-						var actualValue = slider.getRealValue();
-		
-						pic.style.opacity = actualValue / 290;
-									
-						Dom.get(bg).title = "slider value = " + actualValue;
-					});
-
-					slider.subscribe("slideStart", function() {
-							YAHOO.log("slideStart fired", "warn");
-						});
-
-					slider.subscribe("slideEnd", function() {
-							YAHOO.log("slideEnd fired", "warn");
-						});
-
-					// Listen for keystrokes on the form field that displays the
-					// control's value.  While not provided by default, having a
-					// form field with the slider is a good way to help keep your
-					// application accessible.
-					Event.on(textfield, "keydown", function(e) {
-
-						// set the value when the 'return' key is detected
-						if (Event.getCharCode(e) === 13) {
-							var v = parseFloat(this.value, 10);
-							v = (lang.isNumber(v)) ? v : 0;
-
-							// convert the real value into a pixel offset
-							slider.setValue(Math.round(v/scaleFactor));
-						}
-					});
-					
-					// Use setValue to reset the value to white:
-					Event.on("putval", "click", function(e) {
-						slider.setValue(100, false); //false here means to animate if possible
-					});
-					
-					// Use the "get" method to get the current offset from the slider's start
-					// position in pixels.  By applying the scale factor, we can translate this
-					// into a "real value
-					Event.on("getval", "click", function(e) {
-						YAHOO.log("Current value: "   + slider.getValue() + "\n" + 
-								  "Converted value: " + slider.getRealValue(), "info", "example"); 
-					});
-				}
-		
 		
 			(function() {
 				var Event = YAHOO.util.Event,

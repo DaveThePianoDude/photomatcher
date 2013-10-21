8f293e6554578f355e406e86e7b928d2a26128f2
DaveThePianoDude <dah_7@yahoo.com>
2013-10-20 21:22:57 -0400

    streamlined code

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
 	
 				//echo '<img src="data:image/jpg;base64,'.$img_str.'"/>';
 				
-				echo "marker.bindPopup(\x22<h3>".$x."</h3><img id='thenImage".$img_str."' src='https://scontent-a.xx.fbcdn.net/hphotos-ash3/s600x600/1146678_10151933032551834_1629612437_n.jpg' height='240px' width='300px'></img><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
+				echo "marker.bindPopup(\x22<h3>".$x."</h3><img src='data:image/jpg;base64,".$img_str."' height='240px' width='300px'></img><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
 			}
 
 			$x = $x + 1;			

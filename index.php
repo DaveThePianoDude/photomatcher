eeeabc341cc03ac7e56575a484d11a51824da75c
DaveThePianoDude <dah_7@yahoo.com>
2013-10-20 21:34:23 -0400

    streamlined code

--- index.php
+++ index.php
@@ -224,7 +224,7 @@
 	
 				$img_str = trim($line[1]);
 		
-				echo "marker.bindPopup(\x22<h3>Photo Title #".$x."</h3><img src='data:image/jpg;base64,".$img_str."' height='240px' width='300px'></img><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
+				echo "marker.bindPopup(\x22<h3>Photo Title #".$x."</h3><img ID='thenImage12' src='data:image/jpg;base64,".$img_str."' height='240px' width='300px'></img><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
 			}
 
 			$x = $x + 1;			

38ca4ceea222a078b239d9f258eb0c86217979c5
DaveThePianoDude <dah_7@yahoo.com>
2013-10-22 21:08:39 -0400

    modified storage and clear pages

--- clear.php
+++ clear.php
@@ -5,13 +5,16 @@
 		return "dbname=d7qq84ps7u5thb host=ec2-184-73-175-240.compute-1.amazonaws.com port=5432 user=jnnvxxjaenvzor password=Xpq6UHZoub1e6LIUPdUZrX6bSz sslmode=require";
 	}
 	
-		$db = pg_connect(pg_connection_string());
-		
-		$query = "DELETE FROM NOW_PHOTOS WHERE ID > 0";
-		
-		pg_query($db, $query);
-		
-		pg_close($db);
-
+	$db = pg_connect(pg_connection_string());
+	
+	$query = "DELETE FROM NOW_PHOTOS WHERE ID > 0";
+	
+	pg_query($db, $query);
+	
+	$query = "DELETE FROM THEN_PHOTOS WHERE ID > 0";
+	
+	pg_query($db, $query);
+	
+	pg_close($db);
 	
 ?>
---
+++ storethenphoto.php
@@ -0,0 +1,36 @@
+<?php
+
+	function pg_connection_string() {
+
+		return "dbname=d7qq84ps7u5thb host=ec2-184-73-175-240.compute-1.amazonaws.com port=5432 user=jnnvxxjaenvzor password=Xpq6UHZoub1e6LIUPdUZrX6bSz sslmode=require";
+	}
+	
+	if (isset($_FILES['uploadedfile']))
+	{
+		$info = getimagesize($_FILES['uploadedfile']['tmp_name']);
+				
+		# Establish db connection
+		$db = pg_connect(pg_connection_string());
+		
+		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
+		
+		$query = "SELECT * FROM THEN_PHOTOS";
+		
+		$result = pg_query($db, $query);
+		
+		$uid = pg_num_rows($result) + 1;
+		
+		$query = "INSERT INTO THEN_PHOTOS(id, data) Values('$uid', '$data')";
+		
+		pg_query($db, $query);
+		
+		pg_close($db);
+		
+		echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
+	}
+	else
+	{
+		echo 'FILE DOES NOT EXIST';
+	}
+	
+?>

0abce7aded268b885c42bba6ee630787bb655ec8
David Holland <dah_7@yahoo.com>
2015-10-17 17:19:56 -0400

    Added spinner.

--- index.php
+++ index.php
@@ -10,6 +10,19 @@
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
 
 	<script type="text/javascript">
+	$(document).ready(function(){
+		$("#spinner").bind("ajaxSend", function() {
+			$(this).show();
+		}).bind("ajaxStop", function() {
+			$(this).hide();
+		}).bind("ajaxError", function() {
+			$(this).hide();
+		});
+	 
+		 });
+	</script>
+	
+	<script type="text/javascript">
 
 		function prepslides(e) {
 
@@ -231,7 +244,7 @@
 
 				echo "var marker = L.marker([$lat, $lon], {icon: timeIcon, natId:".$uid." }).addTo(map);";
 
-				echo "marker.bindPopup(\x22<h3>Now And Then, #".$uid."</h3><div ID='bigdiv' style='width:320px;height:240px' ><ul class='images'><li><div id="spinner" class="spinner" style="display:none;"><img id="img-spinner" src="ajax-loader.gif" alt="Loading"/></div><img ID='thenImage12' style='opacity:0' src='thenimage".$uid.".jpg' height='240px' width='300px'></img></li><li><img ID='nowImage12' style='opacity:1' src='nowimage".$uid.".jpg' height='240px' width='300px'></img></li></ul></div><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
+				echo "marker.bindPopup(\x22<h3>Now And Then, #".$uid."</h3><div ID='bigdiv' style='width:320px;height:240px' ><ul class='images'><li><div id='spinner' class='spinner' style='display:none;'><img id='img-spinner' src='ajax-loader.gif' alt='Loading'/></div><img ID='thenImage12' style='opacity:0' src='thenimage".$uid.".jpg' height='240px' width='300px'></img></li><li><img ID='nowImage12' style='opacity:1' src='nowimage".$uid.".jpg' height='240px' width='300px'></img></li></ul></div><div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'><div id='slider-thumb' class='yui-slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div></div>\x22).openPopup();";
 			}
 
 			$x = $x + 1;

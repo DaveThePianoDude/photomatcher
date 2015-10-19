b388d5045575dc43ef0116870dab34711c8cf8ee
David Holland <dah_7@yahoo.com>
2015-10-19 19:48:14 -0400

    YUI test3

--- index.php
+++ index.php
@@ -178,7 +178,10 @@
   <body>
 
 	<div id='slider-bg' class='yui-h-slider' tabindex='-1' title='Slider'>
-	<div id='slider-thumb'><img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'></div>
+		<div id='slider-thumb'>
+			<img src='http://yui.yahooapis.com/2.9.0/build/slider/assets/thumb-n.gif'>
+		</div>
+	</div>
 
 	<p>Pixel value: <span id="slider-value">0</span></p>
 

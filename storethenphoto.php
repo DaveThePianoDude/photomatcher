f644117017b110a16bff0110aa6b9bf0ea23c690
David Holland <dah_7@yahoo.com>
2015-11-06 09:41:38 -0500

    Adjusted endpoints for new schema and app upgrade (1).

--- storethenphoto.php
+++ storethenphoto.php
@@ -8,14 +8,14 @@
 		$db = pg_connect(pg_connection_string());
 
 		$uid = uuid_generate_v1mc();
-		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
+		//$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 
-		$query = "INSERT INTO PHOTOS(id, data) Values('$uid', '$data')";
+		$query = "INSERT INTO PHOTOS(id, data) Values('$uid')";
 
 		pg_query($db, $query);
 		pg_close($db);
 
-		echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
+		//echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 	}
 	else
 	{

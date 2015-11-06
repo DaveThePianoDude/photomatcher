1185e07c0aafb584490df7cea114bf5c29046fde
David Holland <dah_7@yahoo.com>
2015-11-06 09:34:04 -0500

    Adjusted endpoints for new schema and app upgrade.

--- storenowphoto.php
+++ storenowphoto.php
@@ -1,36 +1,25 @@
 <?php
 
 	require_once ('db_conn.php');
-	
+
 	if (isset($_FILES['uploadedfile']))
 	{
-		$info = getimagesize($_FILES['uploadedfile']['tmp_name']);
-				
-		//echo $info["mime"]; // will return the mime type
-			
 		# Establish db connection
 		$db = pg_connect(pg_connection_string());
-		
+
+		$uid = uuid_generate_v1mc();
 		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
-		
-		$query = "SELECT * FROM NOW_PHOTOS";
-		
-		$result = pg_query($db, $query);
-		
-		$uid = pg_num_rows($result) + 1;
-		
-		$query = "INSERT INTO NOW_PHOTOS(id, data) Values('$uid', '$data')";
-		
+
+		$query = "INSERT INTO PHOTOS(id, data) Values('$uid', '$data')";
+
 		pg_query($db, $query);
-		
 		pg_close($db);
-		
+
 		echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 	}
 	else
 	{
 		echo 'FILE DOES NOT EXIST';
-		
 	}
-	
+
 ?>
--- storethenphoto.php
+++ storethenphoto.php
@@ -1,33 +1,25 @@
 <?php
 
 	require_once ('db_conn.php');
-	
+
 	if (isset($_FILES['uploadedfile']))
 	{
-		$info = getimagesize($_FILES['uploadedfile']['tmp_name']);
-				
 		# Establish db connection
 		$db = pg_connect(pg_connection_string());
-		
+
+		$uid = uuid_generate_v1mc();
 		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
-		
-		$query = "SELECT * FROM THEN_PHOTOS";
-		
-		$result = pg_query($db, $query);
-		
-		$uid = pg_num_rows($result) + 1;
-		
-		$query = "INSERT INTO THEN_PHOTOS(id, data) Values('$uid', '$data')";
-		
+
+		$query = "INSERT INTO PHOTOS(id, data) Values('$uid', '$data')";
+
 		pg_query($db, $query);
-		
 		pg_close($db);
-		
+
 		echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 	}
 	else
 	{
 		echo 'FILE DOES NOT EXIST';
 	}
-	
+
 ?>

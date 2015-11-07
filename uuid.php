c8455d5878264952f9667e7aa713983a6ac2afcc
David Holland <dah_7@yahoo.com>
2015-11-06 22:24:56 -0500

    TEST

--- uuid.php
+++ uuid.php
@@ -107,15 +107,4 @@ class UUID {
   }
 }
 
-// Usage
-// Named-based UUID.
-
-$v3uuid = UUID::v3('1546058f-5a25-4334-85ae-e68f2a44bbaf', 'SomeRandomString');
-$v5uuid = UUID::v5('1546058f-5a25-4334-85ae-e68f2a44bbaf', 'SomeRandomString');
-
-// Pseudo-random UUID
-
-$v4uuid = UUID::v4();
-
-echo $v4uuid;
 ?>

f87a22fa01d80b50332f8ec6ff6f7672bbbcf59b
David Holland <dah_7@yahoo.com>
2015-11-07 14:44:05 -0500

    Repaired storethenphoto.php.

--- storethenphoto.php
+++ storethenphoto.php
@@ -10,8 +10,9 @@
 
 		$uuid = UUID::v4();
 		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
+		$createdAt = $_GET['createdAt'];
 
-		$query = "INSERT INTO photomatcher.PHOTOS(id) Values('$uuid')";
+		$query = "INSERT INTO photomatcher.PHOTOS(id,data,photo_type,created_at) Values('$uuid','$data','0','$createdAt')";
 
 		pg_query($db, $query);
 		pg_close($db);

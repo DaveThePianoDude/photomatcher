9fcc3d8b7c0385642c7529daa8a528da0f147916
David Holland <dah_7@yahoo.com>
2015-11-06 21:47:02 -0500

    Added ref. to UUID.

--- storenowphoto.php
+++ storenowphoto.php
@@ -8,7 +8,7 @@
 		# Establish db connection
 		$db = pg_connect(pg_connection_string());
 
-		$uid = UUID:v4();
+		$uid = UUID::v4();
 		//$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 
 		//$query = "INSERT INTO PHOTOS(id, data) Values('$uid', '$data')";

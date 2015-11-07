12cf5f863164324f792dc5a3d90de05ca1913042
David Holland <dah_7@yahoo.com>
2015-11-07 11:52:42 -0500

    Added photo_type to endpoints...

--- storephotomatch.php
+++ storephotomatch.php
@@ -10,13 +10,22 @@
 
 	// variables passed via URL:
 	$userName = $_GET['userName'];
-	$createdAt = $_GET['createdAt'];
+	$timestamp = $_GET['createdAt'];
 	$lat = $_GET['lat'];
 	$lon = $_GET['lon'];
 
 	// locally synthesized values:
-	$now_photo = UUID::v4();
-	$then_photo = UUID::v4();
+
+	$query = "SELECT * FROM photomatcher.PHOTOS where created_at = '$timestamp' AND photo_type ='1'";
+	$result = pg_query($db, $query);
+	$line = pg_fetch_row($result);
+	$now_photo = trim($line[0]);
+
+	$query = "SELECT * FROM photomatcher.PHOTOS where created_at = '$timestamp' AND photo_type ='0'";
+	$result = pg_query($db, $query);
+	$line = pg_fetch_row($result);
+	$then_photo = trim($line[0]);
+
 	$description = 'test';
 
 	$query = "INSERT INTO photomatcher.PLACES(id,lat,lon,now_photo,then_photo,description) Values('$uuid','$lat','$lon','$now_photo','$then_photo','$description')";

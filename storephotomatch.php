5cebce4c049913a8a18fc8fc55147d8b0f4cd4e5
David Holland <dah_7@yahoo.com>
2015-11-07 09:52:28 -0500

    Updating association table...

--- storephotomatch.php
+++ storephotomatch.php
@@ -1,47 +1,27 @@
 <?php
-	
+
 	require_once ('db_conn.php');
+	require_once ('uuid.php');
 
 	# Establish db connection
 	$db = pg_connect(pg_connection_string());
-	
-	if (!$db) {
-	
-		echo "Database connection error.";
-		
-		exit;
-		
-	}
-	else
-	{
-		echo "Database connection succeeded.";
-		
-		echo "Now 'n' Then v. 0.127";
-		
-		$query = "CREATE TABLE IF NOT EXISTS Places (natId bigserial primary key NOT NULL, userName varchar(20) NOT NULL, locationId bigserial, photoNowId bigserial, photoThenId bigserial, lat varchar(50) NOT NULL, lon varchar(50) NOT NULL);";
-		
-		pg_query($db, $query);
-	}
-	
-	$query = "SELECT * FROM Places";
-		
-	$result = pg_query($db, $query);
-		
-	$natId = pg_num_rows($result) + 1;
-	
+
+	$uuid = UUID::v4();
+
+	// variables passed via URL:
 	$userName = $_GET['userName'];
-	$locationId = $_GET['locationId'];
-	$photoNowId = $_GET['photoNowId'];
-	$photoThenId = $_GET['photoThenId'];
+	$createdAt = $_GET['createdAt'];
 	$lat = $_GET['lat'];
 	$lon = $_GET['lon'];
 
-	if ($natId != NULL)
-	{
-		$query = "INSERT INTO Places VALUES ($natId, '$userName', $locationId, $photoNowId, $photoThenId, '$lat', '$lon');";
-		
-		echo $query;
-		
-		pg_query($db, $query);
-	}
+	// locally synthesized values:
+	$now_photo = UUID::v4();
+	$then_photo = UUID::v4();
+	$description = 'test';
+
+	$query = "INSERT INTO photomatcher.PLACES(id,lat,lon,now_photo,then_photo,description) Values('$uuid','$lat','$lon','$now_photo','$then_photo','$description')";
+	pg_query($db, $query);
+	pg_close($db);
+
+	echo 'PLACES table updated.';
 ?>

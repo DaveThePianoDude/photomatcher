c1b6c4b83ca1f152f490fd391f260c8d32fb69dc
David Holland <dah_7@yahoo.com>
2015-11-07 12:56:32 -0500

    Added demo file to call Google geo api.

--- update-description.php
+++ update-description.php
@@ -1,6 +1,16 @@
 <?php
 
-	$key = "AIzaSyCJ204tQKkDkj0S-dSohznucBd3C-Hp788";
+	require_once ('db_conn.php');
+
+	# Establish db connection
+	$db = pg_connect(pg_connection_string());
+
+	$query = "SELECT * FROM photomatcher.SETTINGS(value) WHERE name = 'google_api'";
+	$result = pg_query($db, $query);
+	pg_close($db);
+
+	$line = pg_fetch_row($result);
+	$key = trim($line[0]);
 
 	$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=".$key;
 

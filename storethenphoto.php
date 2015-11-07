307616d1bda084136ec0a61356ba9fe490789c1c
David Holland <dah_7@yahoo.com>
2015-11-06 22:30:52 -0500

    Test basics 2.

--- storethenphoto.php
+++ storethenphoto.php
@@ -11,15 +11,14 @@
 		$uuid = UUID::v4();
 		//$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 
-		//$query = "INSERT INTO PHOTOS(id) Values('$uuid')";
+		$query = "INSERT INTO PHOTOS(id) Values('$uuid')";
 
-		//pg_query($db, $query);
+		pg_query($db, $query);
 		pg_close($db);
 
 		//echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
 
 		echo 'ROW ADDED';
-		echo $uuid;
 	//}
 	//else
 	//{

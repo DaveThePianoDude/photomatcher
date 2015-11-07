b00f10f2cec050e4c1ed3dc2b67c991faf2d88c2
David Holland <dah_7@yahoo.com>
2015-11-07 12:43:11 -0500

    Added demo file to call Google geo api.

--- update-description.php
+++ update-description.php
@@ -1,38 +1,8 @@
 <?php
 
-	function CallAPI($method, $url, $data = false)
-	{
-	    $curl = curl_init();
-
-	    switch ($method)
-	    {
-		case "POST":
-		    curl_setopt($curl, CURLOPT_POST, 1);
-
-		    if ($data)
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-		    break;
-		case "PUT":
-		    curl_setopt($curl, CURLOPT_PUT, 1);
-		    break;
-		default:
-		    if ($data)
-			$url = sprintf("%s?%s", $url, http_build_query($data));
-	    }
-
-	    curl_setopt($curl, CURLOPT_URL, $url);
-	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-	    $result = curl_exec($curl);
-
-	    curl_close($curl);
-
-	    return $result;
-	}
-
 	$key = "AIzaSyCJ204tQKkDkj0S-dSohznucBd3C-Hp788";
 
-	$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyCJ204tQKkDkj0S-dSohznucBd3C-Hp788";
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=".$key;
 
 	$response = file_get_contents($url);
 

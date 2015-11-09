db18410aff78d863fa922ca5e8551f6a0d61a9aa
David Holland <dah_7@yahoo.com>
2015-11-08 20:39:30 -0500

    Troubelshoot

--- storenowphoto.php
+++ storenowphoto.php
@@ -1,27 +1,15 @@
 <?php
 
-	require_once ('db_conn.php');
-	require_once ('uuid.php');
-
 	if (isset($_FILES['uploadedfile']))
 	{
-		# Establish db connection
-		$db = pg_connect(pg_connection_string());
-
-		$uuid = UUID::v4();
-		$data = base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
-		$createdAt = $_GET['createdAt'];
-		$length = '256';
-
-		$query = "INSERT INTO photomatcher.PHOTOS(id,data,photo_type,created_at,data_length) Values('$uuid','$data','1','$createdAt','$length')";
-
-		pg_query($db, $query);
-		pg_close($db);
-
-		echo base64_encode(file_get_contents($_FILES['uploadedfile']['tmp_name']));
-
-		echo 'NOW ROW ADDED';
-		echo $uuid;
+		$data = file_get_contents($_FILES['uploadedfile']['tmp_name']);
+		
+		file_put_contents(
+			"testimage.jpg", 
+			base64_decode(
+				str_replace("data:image/jpg;base64", "", $data)
+			)
+		);
 	}
 	else
 	{
